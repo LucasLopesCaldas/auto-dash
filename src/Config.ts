0414b0d904a9195314116dcc149fb71bd48c2f67
Lucas Lopes <lucascaldaslopes15@gmail.com>
2022-10-13 18:02:19 -0300

    refactor: change ref to main

--- src/Config.ts
+++ src/Config.ts
@@ -1,7 +1,7 @@
 export default {
   BASE_URL: 'https://api.github.com/repos/LucasLopesCaldas/dashes/contents/',
   BASE_QUERY: {
-    ref: 'dash-folders',
+    ref: 'main',
   },
   TEMP_FOLDER: '/tmp/',
 };
